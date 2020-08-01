ec4c141daf876b51fdc17894dfb4d292236998ec
pandavandama <strangemikados@gmail.com>
2020-08-01 18:07:42 +0300

    fsaf

--- src/main/java/help/Main.java
+++ src/main/java/help/Main.java
@@ -1,7 +1,10 @@
 package help;
+import com.google.gson.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Help! i need somebody!");
+        System.out.println("Help! I need somebody!");
+        Gson g = new Gson();
     }
+
 }
